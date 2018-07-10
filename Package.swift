1c7824cafb522031a351ece348af80802f7d93f4
giginet <giginet.net@gmail.com>
2018-07-11 04:21:00 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,10 +9,6 @@ let package = Package(
         .brew(["libjwt"]),
         .apt(["libjwt"]),
     ],
-    dependencies: [
-        .package(url: "git@github.com:giginet/jansson-Swift.git", from: "1.0.0"),
-        .package(url: "git@github.com:IBM-Swift/OpenSSL.git", from: "1.0.0"),
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,7 +16,15 @@ let package = Package(
             targets: ["JWT"]
         )
     ],
+    dependencies: [
+        .package(url: "git@github.com:giginet/jansson-Swift.git", from: "1.0.0"),
+        .package(url: "git@github.com:IBM-Swift/OpenSSL.git", from: "1.0.0"),
+    ],
     targets: [
-        .target(name: "JWT")
+        .target(name: "JWT",
+                dependencies: [
+                    "OpenSSL",
+                    "Jansson"
+                ])
     ]
 )
